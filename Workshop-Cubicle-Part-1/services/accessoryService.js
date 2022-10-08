fa03c5aa2842275c112e1897f60e9d477e7f7728
ViktorAtanasof <atanasowviktor@gmail.com>
2022-10-08 19:42:06 +0300

    Structuring code

--- Workshop-Cubicle-Part-1/services/accessoryService.js
+++ Workshop-Cubicle-Part-1/services/accessoryService.js
@@ -1,4 +1,5 @@
 const Accessory = require('../models/Accessory');
+const Cube = require('../models/Cube');
 
 async function getAllAccessories() {
     return Accessory.find({}).lean();
@@ -13,7 +14,20 @@ async function createAccessory(name, description, imgUrl) {
 }
 
 async function addAccessories(cubeId, accessoryIds) {
+    const cube = await Cube.findById(cubeId);
+    const accessories = await Accessory.find({ _id: { $in: accessoryIds } });
 
+    const toRemove = cube.accessories.filter(c => accessories.every(x => x._id != c._id));
+
+    const newlyAdded = accessories.filter(c => cube.accessories.every(x => x._id != c._id));
+
+    newlyAdded.forEach(c => {
+        cube.accessories.push(c);
+        c.cubes.push(cube);
+    });
+
+    await cube.save();
+    await Promise.all(newlyAdded.map(c => c.save()));
 }
 
 module.exports = {
