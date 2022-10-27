1fc4eada1f801504f005aedf9591c5fc0341c62f
ViktorAtanasof <atanasowviktor@gmail.com>
2022-10-27 19:15:26 +0300

    File fixes

--- Exam-Preparations/Tutorials/Scaffold/config/routes.js
+++
@@ -1,7 +0,0 @@
-const authController = require("../controllers/authController");
-const homeController = require("../controllers/homeController")
-
-module.exports = (app) => {
-    app.use('/', homeController);
-    app.use(authController);
-}
